ab4ca45afff4b665b4661abbceae519fd67d21cd
Danny Bowie <kremzan17@gmail.com>
2024-10-21 14:42:38 -0400

    adding firebase

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCxsEV7XU95eMCcYaWTQsr_1lpjlBOVOJ4",
+    authDomain: "swe-6623.firebaseapp.com",
+    projectId: "swe-6623",
+    storageBucket: "swe-6623.firebaseapp.com",
+    messagingSenderId: "1054721593811",
+    appId: "1:1054721593811:web:bf143b1426f42309dc94e3",
+    measurementId: "G-1250NG4CKM"
+  };
+
+
+
+we
