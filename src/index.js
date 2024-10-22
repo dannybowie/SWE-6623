622347662a40e4abb46f9a1eca8a91f94775adc4
Danny Bowie <kremzan17@gmail.com>
2024-10-22 15:32:37 -0400

    adding more for firebase confi

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+import { getDownloadURL } from "firebase/storage";
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCxsEV7XU95eMCcYaWTQsr_1lpjlBOVOJ4",
-    authDomain: "swe-6623.firebaseapp.com",
-    projectId: "swe-6623",
-    storageBucket: "swe-6623.firebaseapp.com",
-    messagingSenderId: "1054721593811",
-    appId: "1:1054721593811:web:bf143b1426f42309dc94e3",
-    measurementId: "G-1250NG4CKM"
-  };
+  apiKey: "AIzaSyCxsEV7XU95eMCcYaWTQsr_1lpjlBOVOJ4",
+  authDomain: "ksu-swe-project.firebaseapp.com",
+  projectId: "ksu-swe-project",
+  storageBucket: "ksu-swe-project.appspot.com",
+  messagingSenderId: "990956535258",
+  appId: "1:990956535258:web:bf143b1426f42309dc94e3",
+  measurementId: "G-1250NG4CKM"
+};
 
-
-
-we
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const storage =  getStorage();
+const storageRef = ref(storage);
